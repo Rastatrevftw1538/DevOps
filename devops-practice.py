9031a38bc164e1318afcdbe8779300cc9f1be643
Rastatrevftw1538 <trevor.cardoza1538@gmail.com>
2019-05-04 11:06:04 -0700

    First DevOps practice code

---
+++ devops-practice.py
@@ -0,0 +1,17 @@
+def main ():
+
+   a =  input("UPPER insert name: ")
+   print(a.upper())
+
+   b = input("LEN insert long word: ")
+   print(len(b))
+
+   c = input("COUNT insert a word with I's: ")
+   print(c.count("i"))
+
+   d = input("STRIP insert a word with y: ")
+   print(d.strip("y"))
+
+   e = input(("JOIN insert a bunch of numbers: "))
+   z = ","
+   print(z.join(e))
