b9d4a8582e2c168f2ef3c22691090f16c6111bab
Trevor Cardoza <trevor.cardoza1538@gmail.com>
2019-08-17 10:26:06 -0700

    Terraform installation

---
+++ Terraform-install.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get --assume-yes install python
+sudo apt-get --assume-yes install python-pip
+sudo curl -O https://releases.hashicorp.com/terraform/0.12.6/terraform_0.12.6_linux_amd64.zip
+sudo apt-get --assume-yes install unzip
+sudo mkdir /bin/terraform
